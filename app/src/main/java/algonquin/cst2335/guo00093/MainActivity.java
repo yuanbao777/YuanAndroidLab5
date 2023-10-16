3d0ccb69eb36e7c716f7ec817c8ae931afbe2f0d
Yuan Guo <guo00093@algonquinlive.com>
2023-10-16 15:01:23 -0400

    Week Password

--- app/src/main/java/algonquin/cst2335/guo00093/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/guo00093/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.guo00093;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editTextTextPassword);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(clk -> {
+            String password = et.getText().toString();
+
+            checkPasswordComplexity (password);
+
+            void checkPasswordComplexity (String pw);{
+
+            }
+        });
     }
 }
